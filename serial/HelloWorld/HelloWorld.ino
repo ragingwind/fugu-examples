ea464d6c48620e488612f033f5cbbad198cbb610
Jimmy Moon <ragingwind@gmail.com>
2020-10-05 07:42:30 +0900

    Add hello world example

---
+++ serial/HelloWorld/HelloWorld.ino
@@ -0,0 +1,31 @@
+/*
+  Keyboard test
+
+  For the Arduino Leonardo, Micro or Due
+
+  Reads a byte from the serial port, sends a keystroke back.
+  The sent keystroke is one higher than what's received, e.g. if you send a,
+  you get b, send A you get B, and so forth.
+
+  The circuit:
+  - none
+
+  created 21 Oct 2011
+  modified 27 Mar 2012
+  by Tom Igoe
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/KeyboardSerial
+*/
+
+
+void setup() {
+  // open the serial port:
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.write("Hello World");
+  delay(1000);
+}
